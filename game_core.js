21b62d827178f88522df1bf696738b59c6f3d80e
xisquinha <96737387+xisquinha@users.noreply.github.com>
2024-10-03 00:25:03 +0100

    Update game_core.js

--- game_core.js
+++ game_core.js
@@ -1,17 +1,20 @@
 var config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  
+  scale: {
+      parent: 'game',
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+      width: 800,
+      height: 600
+  },
+  
   physics: {
       default: 'arcade',
       arcade: {
           gravity: { y: 300 },
           debug: false
       }
-  },
-  scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
   },
   scene: {
       preload: preload,
